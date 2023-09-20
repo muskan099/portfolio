3cb2601d8c0de92a45e9b0c4377592382a605715
your_github_muskan09 <your_github_muskankapoorrr@gmail.com>
2023-09-20 13:01:09 +0530

    changes

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -2,7 +2,6 @@ import './index.scss';
 import LogoS from '../../../assets/images/logo-s.png';
 import { useEffect, useRef } from 'react';
 import gsap from 'gsap-trial';
-import DrawSVGPlugin from "gsap-trial/DrawSVGPlugin";
 
 
 
@@ -13,18 +12,6 @@ const Logo = () => {
     const solidLogoRef = useRef();
 
     useEffect(() => {
-      // gsap.registerPlugin(DrawSVGPlugin)
-
-      gsap.timeline()
-      .to(bgRef.current, {
-       duration:1,
-       opacity:1
-      })
-      .from( outlineLogoRef.current, {
-        drawSVG:0,
-        duration:20,
-      })
-
       gsap.fromTo(
         solidLogoRef.current,{
           opacity:0,
